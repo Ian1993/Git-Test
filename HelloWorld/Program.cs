72e6950d860cebe730c3705277f2d119f29953da
Ian Nevills <ianmnevi@ut.utm.edu>
2018-01-25 11:07:06 -0600

    Working Random Number game

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,8 +10,68 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            int userNum = 0;
 
-            Console.WriteLine("Hello World!");
+            int count = 0;
+
+            Random num = new Random();
+
+            int randomNum = num.Next(0, 101);
+
+            Console.WriteLine("I am thinking of a random number between 0 and 100 (100 included");
+
+            Console.WriteLine("Type your guess");
+
+            userNum = Convert.ToInt32(Console.ReadLine());
+
+            numberGame(randomNum, userNum, count);
+
+            
+
+        }
+
+        static void numberGame(int randomNum, int userNum, int count)
+        {
+            
+            if(userNum < randomNum)
+            {
+                Console.WriteLine("Too low");
+                Console.WriteLine("Try again, enter another Number");
+
+                userNum = Convert.ToInt32(Console.ReadLine());
+
+                count = count + 1;
+
+                numberGame(randomNum, userNum, count);
+            }
+
+            else if(userNum > randomNum)
+            {
+                Console.WriteLine("Too High");
+                Console.WriteLine("Try again, enter another Number");
+
+                userNum = Convert.ToInt32(Console.ReadLine());
+
+                count = count + 1;
+
+                numberGame(randomNum, userNum, count);
+
+            }
+
+            else
+            {
+                Console.WriteLine("Correct!");
+
+                count = count + 1;
+
+                Console.WriteLine("Tries: " + count);
+
+                Console.WriteLine("Press any number to exit");
+
+                userNum = Convert.ToInt32(Console.ReadLine());
+
+
+            }
 
         }
     }
